3b1325de6c0849c772ec39c59a71e38ff612f1c5
Matthew Rada <matthewr7.christopher@gmail.com>
2023-11-13 19:52:58 -0500

    constants for object limit

--- pcr/forms.py
+++ pcr/forms.py
@@ -2,13 +2,10 @@ from django.forms import ModelForm
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+
 from django.contrib.auth.models import User
 from .models import *
-
-
-# **CONSTANTS** #
-BATCH_LIMIT = 5
-# **CONSTANTS** #
+from .constants import *
 
 
 # **INVENTORY** #
