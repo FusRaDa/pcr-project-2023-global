886f98d2f9460870c4166a4305d9d2b6b01fded2
Matthew Rada <matthewr7.christopher@gmail.com>
2023-12-07 20:03:53 -0500

    work on stripe integration subscriptions

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,17 +9,31 @@ class CreateUserForm(UserCreationForm):
   def clean(self):
     cleaned_data = super().clean()
     email = cleaned_data.get('email')
+    first_name = cleaned_data.get('first_name')
+    last_name = cleaned_data.get('last_name')
 
     if not email:
       raise ValidationError(
         ({'email': ["Please enter an email."]})
       )
+    
+    if not first_name:
+      raise ValidationError(
+        ({'first_name': ["Please enter your first name."]})
+      )
+    
+    if not last_name:
+      raise ValidationError(
+        ({'last_name': ["Please enter your last name."]})
+      )
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.fields['email'].required = True
+    self.fields['first_name'].required = True
+    self.fields['last_name'].required = True
 
   class Meta: 
     model = User
-    fields = ['username', 'email', 'password1', 'password2']
+    fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
