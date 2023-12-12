8e1951f9aee3861c9cbdff92ec30882957647770
Matthew Rada <matthewr7.christopher@gmail.com>
2023-12-11 21:13:38 -0500

    work on modelformset in items_views

--- store/views/affiliates_views.py
+++ store/views/affiliates_views.py
@@ -1,14 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 
+from ..models.affiliates import Brand, Contact
 from ..forms.affiliates import BrandForm, ContactForm
+
+
+@staff_member_required(login_url='login')
+def brands(request):
+  brands = Brand.objects.all()
+  context = {'brands': brands}
+  return render(request, 'affiliates/brands.html', context)
+
 
 @staff_member_required(login_url='login')
 def create_brand(request):
-
   form = BrandForm()
+  if request.method == "POST":
+    form = BrandForm(request.POST)
+    if form.is_valid():
+      form.save()
 
-
-
-  context = {}
+  context = {'form': form}
   return render(request, 'affiliates/create_brand.html', context)
+
+
+@staff_member_required(login_url='login')
+def contacts(request):
+  contacts = Contact.objects.all()
+  context = {'contacts': contacts}
+  return render(request, 'affiliates/contacts.html', context)
+
+
+@staff_member_required(login_url='login')
+def create_contact(request):
+  form = ContactForm()
+  if request.method == "POST":
+    form = ContactForm(request.POST)
+    if form.is_valid():
+      form.save()
+  
+  context = {'form': form}
+  return render(request, 'affiliates/create_contact.html', context)
--- store/views/items_views.py
+++ store/views/items_views.py
@@ -6,11 +6,21 @@ from django.contrib import messages
 from ..models.items import Kit, StorePlate, StoreReagent, StoreTube
 from ..forms.items import KitForm
 
+@staff_member_required(login_url='login')
+def kits(request):
+  kits = Kit.objects.all()
+  context = {'kits': kits}
+  return render(request, 'items/kits.html', context)
+
 
 @staff_member_required(login_url='login')
 def create_kit(request):
-
   form = KitForm()
   
   context = {'form': form}
   return render(request, 'items/create_kit.html', context)
+
+
+@staff_member_required(login_url='login')
+def items_in_kit(request):
+  pass
