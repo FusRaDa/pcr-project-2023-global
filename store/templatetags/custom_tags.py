ac9bfd5d8b7a3ce17215a9f44bb2b420f5e06f6f
Matthew Rada <matthewr7.christopher@gmail.com>
2023-12-18 16:55:05 -0500

    custom filters ftw

---
+++ store/templatetags/custom_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from decimal import Decimal
+
+register = template.Library()
+
+def dec_mult(value, arg):
+  product = Decimal(value*arg)
+  return product
+
+register.filter("dec_mult", dec_mult)
