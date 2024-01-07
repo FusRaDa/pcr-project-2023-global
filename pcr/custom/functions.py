600c7f4d119dee49099e9b2c1c74c5e2f42cc488
Matthew Rada <matthewr7.christopher@gmail.com>
2024-01-06 20:28:07 -0500

    positions

--- pcr/custom/functions.py
+++ pcr/custom/functions.py
@@ -1,5 +1,7 @@
 from ..models.batch import Batch, Sample
 from ..models.assay import Assay
+from ..models.inventory import Plate
+import math
 
 
 def create_samples(number_of_samples, lab_id, user):
@@ -48,11 +50,11 @@ def samples_by_assay(samples):
 
 
 # order samples by DNA PCR
-def dna_pcr_samples_by_assay(assay_samples): 
+def dna_pcr_samples(assay_samples): 
   colors = ['table-primary', 'table-secondary', 'table-success', 'table-danger', 'table-warning', 'table-info', 'table-light', 'table-dark']
 
   # collect all samples that are for DNA in PCR by assay
-  dna_pcr_samples = []
+  all_samples = []
   for index in assay_samples:
     for assay, samples in index.items():
       if assay.method == Assay.Methods.PCR and assay.type == Assay.Types.DNA:
@@ -73,13 +75,31 @@ def dna_pcr_samples_by_assay(assay_samples):
         if color > 7:
           color = 0
         assay_data = {assay: data}
-        dna_pcr_samples.append(assay_data)
+        all_samples.append(assay_data)
 
-  return dna_pcr_samples
+  return all_samples
 
 
-# compact plate method
-def json_dna_pcr(dna_pcr_samples, process):
+def rna_pcr_samples(assay_samples):
+  pass
+
+
+def dna_qpcr_samples(assay_samples):
+  pass
+
+
+def rna_qpcr_samples(assay_samples):
+  pass
+
+
+# 4 methods to load plate:
+# 1 - compact & organized horizontally
+# 2 - compact & organized vertically
+# 3 - fully compact horizontally
+# 4 - fully compact vertically
+
+# compact & organized plate method - horizontal
+def compact_organized_horizontal(dna_pcr_samples, process):
   # list plates from smallest to greatest size
   plates = []
   for plate in process.plate.all().order_by('size'):
@@ -96,36 +116,73 @@ def json_dna_pcr(dna_pcr_samples, process):
     if plate.size > total_wells_used:
       remaining_wells = plate.size
 
+      # create dictionaries
       plate_data = {'plate': plate}
       protocol_data = {'protocol': process.pcr_dna_protocol}
       samples_data = {'samples': []}
       
-      position = 1
+      position = 0
       for data in dna_pcr_samples:
         for assay, samples in data.items():
           control_color = samples[0]['color']
-       
-          for sample in samples:
-            sample['position'] = position
-            samples_data['samples'].append(sample)
 
-            position += 1
-            remaining_wells -= 1
+          sample_wells = len(samples)
+          control_wells = assay.controls.count()
+          total_wells = sample_wells + control_wells
 
-          for s in samples_data['samples']:
-            if s in samples:
-              samples.remove(s)
-          
-          # if len(samples) > 0:
-          #   for control in assay.controls.all():
-          #     control_data = {
-          #       'position': None,
-          #       'color': control_color,
-          #       'lab_id': control.name,
-          #       'sample_id': control.lot_number,
-          #       'assay': assay
-          #     }
-          #     samples.append(control_data)
+          if total_wells < remaining_wells:
+              
+            for sample in samples:
+              position += 1
+              sample['position'] = position
+              samples_data['samples'].append(sample)
+              
+
+            # add validation if plate size is insufficient to even hold only one assay w/ controls
+            if plate.size == Plate.Sizes.EIGHT:
+              print('plate 8...')
+              pass
+
+            if plate.size == Plate.Sizes.FOURTY_EIGHT:
+              
+              # calculate remaining wells in row by first identifying row that well/position is currently
+              wells_in_row = 6
+              row = math.floor(position / wells_in_row) + 1
+              if position % wells_in_row == 0:
+                row -= 1
+              rem_wells = (row * wells_in_row) - position
+
+              if control_wells < rem_wells:
+                for control in assay.controls.all():
+                  position += 1
+                  control_data = {
+                    'position': position,
+                    'color': control_color,
+                    'lab_id': control.name,
+                    'sample_id': control.lot_number,
+                    'assay': assay
+                  }
+                  samples.append(control_data)
+              else:
+                # go to next row and add controls there with positions reeee!
+                pass
+
+              
+        
+              pass
+
+            if plate.size == Plate.Sizes.NINETY_SIX:
+              pass
+
+            if plate.size == Plate.Sizes.THREE_HUNDRED_EIGHTY_FOUR:
+              pass
+            
+
+
+      # remove samples from list that have already been loaded into plate
+      for s in samples_data['samples']:
+        if s in samples:
+          samples.remove(s)
 
       plate_dict = protocol_data | plate_data | samples_data
    
@@ -133,74 +190,3 @@ def json_dna_pcr(dna_pcr_samples, process):
     else:
       pass
 
- 
-  
- 
- 
-
-
-
-  
- 
-     
-
-  
-
-  
-  
-      
-    
-  
-
-
-
-
-
-
-
-  
-
-
-
-  
-
-
- 
-
-  # plate = {
-  #   'protocol': {
-  #     'name': name,
-  #     'denature': denature,
-  #     'denature_temp': denature_temp,
-  #     'denature_duration': denature_duration,
-  #     'anneal_temp': anneal_temp,
-  #     'anneal_duration': anneal_duration,
-  #     'extension_temp': extension_temp,
-  #     'extension_duration': extension_duration,
-  #     'number_of_cycles': number_of_cycles,
-  #   },
-
-  #   'samples': [
-  #     {
-  #       'color': color,
-  #       'lab_id': lab_id,
-  #       'sample_id': sample_id,
-  #       'assay': assay
-  #     },
-
-  #     {
-  #       'color': color,
-  #       'lab_id': lab_id,
-  #       'sample_id': sample_id,
-  #       'assay': assay
-  #     },
-      
-  #     {
-  #       'color': color,
-  #       'lab_id': lab_id,
-  #       'sample_id': sample_id,
-  #       'assay': assay
-  #     },
-      
-  #   ]
-  # }
