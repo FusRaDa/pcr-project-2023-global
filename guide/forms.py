2c9769cf3f0b471dac18709397d4e3e55d2911f8
Matthew Rada <matthewr7.christopher@gmail.com>
2024-02-07 12:00:07 -0500

    html changes

--- guide/forms.py
+++ guide/forms.py
@@ -5,6 +5,17 @@ from store.models.affiliates import Contact
 
 class ContactForm(ModelForm):
 
+  def __init__(self, *args, **kwargs):
+    self.user = kwargs.pop('user')
+    super().__init__(*args, **kwargs) 
+ 
+    self.fields['brand'].widget.attrs['class'] = 'form-control'
+    self.fields['company'].widget.attrs['class'] = 'form-control'
+    self.fields['first_name'].widget.attrs['class'] = 'form-control'
+    self.fields['last_name'].widget.attrs['class'] = 'form-control'
+    self.fields['email'].widget.attrs['class'] = 'form-control'
+    self.fields['phone_number'].widget.attrs['class'] = 'form-control'
+
   class Meta:
     model = Contact
     fields = '__all__'
