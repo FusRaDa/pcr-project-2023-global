3179ee6073ffced8ec941a123ef93cc43a8853ba
Matthew Rada <matthewr7.christopher@gmail.com>
2024-02-07 19:23:38 -0500

    work on landing page

--- pcr/custom/constants.py
+++ pcr/custom/constants.py
@@ -1,9 +1,12 @@
 # This files keeps all enums as limits to how many object can be created per user. Premium tier will be unlimited!
 
 class LIMITS:
+  # DEPENDANT LIMITS
   ASSAY_LIMIT = 20
   ASSAY_CODE_LIMIT = 30
   BATCH_LIMIT = 1000
   PROCESS_LIMIT = 1500
 
+  # INDEPENDANT LIMITS
   SAMPLES_PER_BATCH_LIMIT = 100
+  
