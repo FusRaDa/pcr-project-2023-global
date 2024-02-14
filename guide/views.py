58378187fa1bb45bd011cbd44831f6e353d9669a
Matthew Rada <matthewr7.christopher@gmail.com>
2024-02-14 16:00:27 -0500

    work on FAQ

--- guide/views.py
+++ guide/views.py
@@ -8,3 +8,8 @@ def landing_page(request):
   brands = Brand.objects.all()
   context = {'brands': brands}
   return render(request, 'landing_page.html', context)
+
+
+def about_page(request):
+  context = {}
+  return render(request, 'about_page.html', context)
